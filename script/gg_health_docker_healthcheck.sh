4c90477e0989bdb7d21d7460787b69cb6c4893e8
Ryan Taylor Long <ryan@rtlong.com>
2016-09-19 22:32:30 +0100

    Add identifying User-Agent to gg_health_check script
    [no story]

--- script/gg_health_docker_healthcheck.sh
+++ script/gg_health_docker_healthcheck.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 URL_PORT="${1:-3000}"
 URL_PATH="${2:-/health/status}"
 
-curl --fail --silent --show-error "http://localhost:${URL_PORT}${URL_PATH}" || exit 1
+curl --fail --silent --show-error -H 'User-Agent: com.goodguide.docker_base_image.internal_health_check/v1' "http://localhost:${URL_PORT}${URL_PATH}" || exit 1
